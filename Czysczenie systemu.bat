8c0f20229948e6be2be3f56c247348e7a0ae5e3f
Chinchillus <91843144+Chinchillus@users.noreply.github.com>
2023-03-20 19:19:50 +0100

    Update Czysczenie systemu.bat

--- Czysczenie systemu.bat
+++ Czysczenie systemu.bat
@@ -1,14 +1,14 @@
 @echo off
 rem Script written by Chinchill#5925 on Discord
-rem Lekkie zmianki, dodanie czyszczenia wszelkich logow w systemie, usuniecie 3 linijek kodu ktore nic nie robily
+rem Poprawa czytelnosci kodu, usuniecie syfu ktory nic nie robil, zmiana kodu w taki sposob ze nie pokazuje bledow podczas usuwania
 
 rem Start jako administrator bez menu kontekstowego
 set "params=%*"
-cd /d "%~dp0" && ( if exist "%Temp%\getadmin.vbs" del "%Temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%Temp%\getadmin.vbs" && "%Temp%\getadmin.vbs" && exit /B )
+cd /d "%~dp0" && ( if exist "%Temp%\getadmin.vbs" del "%Temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul  || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%Temp%\getadmin.vbs" && "%Temp%\getadmin.vbs" && exit /B )
 
-rem Kolejno: tytul, kolor czerowny 12.6 poprzednia
-title Czysczenie systemu (wersja 12.7 "Modularne krzeslo")
-color c
+rem Kolejno: tytul okna, kolor blekitny "12.7.1 Dwa modularne krzesla" poprzednia
+title Czysczenie systemu (wersja 12.7.2 "Trzy modularne krzesla")
+color b
 
 rem Sprawdzenie czy uzytkownik posiada uprawnienia administratora (uzywane w momencie gdy skrypt wyzej sie wysypie)
 timeout /t 0 /nobreak > NUL
@@ -42,28 +42,21 @@ if %errorlevel%==0 (
  echo    Y88888P     dP     Y88888P      Y88888P' 88888888P  88888888P 88     88  dP     dP  88888888P  dP     dP    
  echo.
  echo.
- echo Witaj %USERNAME%
-
-
-
-rem Czysczenie kosza
-rd /s /q c:\$Recycle.Bin 
-
-rem Czysczenie logów | nie zostalo uzyte cd C/ bo strasznie spowalnia skrypt szukajac plikow w WinSxS
-cd %HomePath%
-del *.log /a /s /q /f
-cd %ProgramFiles%
-del *.log /a /s /q /f
-cd %ProgramFiles(x86)%
-del *.log /a /s /q /f
-
+ echo Witaj, %USERNAME%
+ 
+rem Poprzednia wersja tego czegos nie brala pod uwage tego ze to ma dzialac dobrze a nie szybko
 color c
+cd \
+del *.log /a /s /q /f 2>nul
+cd C:
 
-rem Usuwanie Tempu prefetchu itp.
-del /s /f /q %WinDir%\Prefetch\*.* 
-del /s /f /q %Temp%\*.* 
-del /s /f /q %AppData%\Temp\*.* 
-del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.* 
+
+color a
+rem Główne czyszczenie
+del /s /f /q %WinDir%\Prefetch\*.* 2>nul 
+del /s /f /q %Temp%\*.* 2>nul 
+del /s /f /q %AppData%\Temp\*.* 2>nul 
+del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.* 2>nul 
 rd /s /q %WinDir%\Prefetch 
 rd /s /q %Temp% 
 rd /s /q %AppData%\Temp 
@@ -80,9 +73,12 @@ cd C:/
 color c
 
 rem Crash dumpy
-del /s /f /q %LocalAppData%\CrashDumps\*.*
+del /s /f /q %LocalAppData%\CrashDumps\*.* 2>nul 
 cd C:/
-color 2
+color 8
+
+
+rem Pojedyncze aplikacje o ktorych wiem
 
 rem Betterdiscord (niepotrzebne pliki instalatora)
 cd %AppData%
@@ -108,19 +104,19 @@ rem Spotify
 cd %LocalAppData%\Spotify
 rd /s /q Data
 cd Browser
-del /s /f /q Cache\Cache_Data\*.* 
-del /s /f /q DawnCache\*.* 
-del /s /f /q GPUCache\*.* 
+del /s /f /q Cache\Cache_Data\*.* 2>nul 
+del /s /f /q DawnCache\*.* 2>nul 
+del /s /f /q GPUCache\*.* 2>nul 
 rd /s /q "Service Worker"\CacheStorage 
 rd /s /q "Service Worker"\ScriptCache 
 rd /s /q "Code Cache"\js
-color 9
+color e
 rem Cache google
 cd %LocalAppData%\Google\Chrome\User Data\Default
 rd /s /q "File System" 
-del /s /f /q Cache\Cache_Data\*.* 
-del /s /f /q DawnCache\*.* 
-del /s /f /q GPUCache\*.* 
+del /s /f /q Cache\Cache_Data\*.* 2>nul 
+del /s /f /q DawnCache\*.* 2>nul 
+del /s /f /q GPUCache\*.* 2>nul 
 rd /s /q "Service Worker"\CacheStorage 
 rd /s /q "Service Worker"\ScriptCache 
 rd /s /q "Code Cache"\js 
