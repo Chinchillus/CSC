c778df3e061a15349fb13136f5b53292995d523b
Chinchillus <91843144+Chinchillus@users.noreply.github.com>
2023-05-26 16:31:25 +0200

    Update Czysczenie systemu.bat

--- Czysczenie systemu.bat
+++ Czysczenie systemu.bat
@@ -1,39 +1,37 @@
+rem if u are stealing my code please just add me to the credit @Chinchill#5925 (Discord)
+cls
 @echo off
 color c
 MODE 170,40
-rem Script written by Chinchill#5925
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%Temp%\getadmin.vbs" del "%Temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul  || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%Temp%\getadmin.vbs" && "%Temp%\getadmin.vbs" && exit /B )
-
-title Czysczenie systemu (wersja 14 "Drzwi hybrydowe")
+title CSC (v14.1 "Szklane drzwi hybrydowe")
 :start
-echo.                                                             
-echo.                                                              
+echo.
+echo.
 echo        CCCCCCCCCCCCC     SSSSSSSSSSSSSSS         CCCCCCCCCCCCC
 echo      CCC::::::::::::C  SS:::::::::::::::S      CCC::::::::::::C
 echo    CC:::::::::::::::C S:::::SSSSSS::::::S    CC:::::::::::::::C
 echo   C:::::CCCCCCCC::::C S:::::S     SSSSSSS   C:::::CCCCCCCC::::C
 echo  C:::::C       CCCCCC S:::::S              C:::::C       CCCCCC
-echo C:::::C               S:::::S             C:::::C              
-echo C:::::C                S::::SSSS          C:::::C              
-echo C:::::C                 SS::::::SSSSS     C:::::C              
-echo C:::::C                   SSS::::::::SS   C:::::C              
-echo C:::::C                      SSSSSS::::S  C:::::C              
-echo C:::::C                           S:::::S C:::::C              
+echo C:::::C               S:::::S             C:::::C
+echo C:::::C                S::::SSSS          C:::::C
+echo C:::::C                 SS::::::SSSSS     C:::::C
+echo C:::::C                   SSS::::::::SS   C:::::C
+echo C:::::C                      SSSSSS::::S  C:::::C
+echo C:::::C                           S:::::S C:::::C
 echo  C:::::C       CCCCCC             S:::::S  C:::::C       CCCCCC
 echo   C:::::CCCCCCCC::::C SSSSSSS     S:::::S   C:::::CCCCCCCC::::C
 echo    CC:::::::::::::::C S::::::SSSSSS:::::S    CC:::::::::::::::C
 echo      CCC::::::::::::C S:::::::::::::::SS       CCC::::::::::::C
 echo        CCCCCCCCCCCCC   SSSSSSSSSSSSSSS           CCCCCCCCCCCCC
-echo.                                                                                                                           
+echo.
 echo Witaj, %USERNAME%!
 echo.
-timeout 1 >nul
+timeout /t 1 /nobreak >nul
 goto csc
 :csc
-cls
-rem SLJ BTG   
-rem Cofnieto zmiane
+cls 
 echo Logi
 del %HomePath%\AppData\LocalLow\*.log /a /s /q /f 2>nul
 del %HomePath%\AppData\Roaming\*.log /a /s /q /f 2>nul
@@ -50,10 +48,7 @@ del %WinDir%\setupact.log
 del %WinDir%\setuperr.log
 cd C:
 rd /s /q C:\$Recycle.bin
-rem Jakas tam naprawa kosza z Visty i Windowsa 7 (czasami pomaga na nowszych windowsach) usuwa pliki z kosza
-
 color a
-rem Główne czyszczenie
 echo Wszystko i nic
 del /s /f /q %WinDir%\Prefetch\*.* 2>nul 
 del /s /f /q %Temp%\*.* 2>nul 
@@ -72,47 +67,40 @@ md %Temp%
 md %AppData%\Temp 
 md %HomePath%\AppData\LocalLow\Temp 
 cd C:/
-rem Crash dumpy
 echo Crash dumpy
 del /s /f /q %LocalAppData%\CrashDumps\*.* 2>nul 
-rem DirectX
 echo DirectX
 rd /s /q %LocalAppData%\D3DSCache 2>nul
 md %LocalAppData%\D3DSCache 2>nul
 cd C:/
 color 8
-
-rem Pojedyncze aplikacje o ktorych wiem (wahahahah bocik to gej)
-rem BetterDiscord, bezużyteczne pliki instalatora
 echo Betterdiscord
 cd %AppData%
 rd /s /q "BetterDiscord Installer" 
-rem cache Crystal Launchera
+del /s /f /q obs-studio\logs\*.* 2>nul
+del /s /f /q obs-studio\crashes\*.* 2>nul
+rd /s /q obs-studio\plugin_config\obs-browser\Cache
+rd /s /q obs-studio\plugin_config\obs-browser\"Code Cache"
+rd /s /q obs-studio\plugin_config\obs-browser\GPUCache
 echo Crystal Launcher
 rd /s /q Crystal-Launcher\cache  
-rem Cache Discorda
 echo Discord
 rd /s /q discord\Cache
 rd /s /q discord\"Code Cache"\js
 rd /s /q discord\GPUCache
-rem Cache Lunar Client
 echo Lunar Client
 rd /s /q lunarclient\Cache 
-rem Crash reporty z Minecraft
 echo Minecraft
 del /s /f /q %appdata%\.minecraft\crash-reports\*.* 2>nul
-rem Logitech G-HUB
 echo Logitech G-HUB
 rd /s /q LGHUB\Cache\Cache_Data
 rd /s /q LGHUB\"Code Cache"\js
 rd /s /q LGHUB\GPUCache
-rem Steelseries GG
 echo Steelseries GG
 rd /s /q steelseries-gg-client\Cache
 rd /s /q steelseries-gg-client\"Code Cache"\js
 rd /s /q steelseries-gg-client\GPUCache
 color a
-rem Syf ze Spotify
 echo Spotify
 cd %LocalAppData%\Spotify
 rd /s /q Data
